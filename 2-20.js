a694090ed720e81237e028a4edd7eba81c11952e
liguojuan <1562928196@qq.com>
2016-08-25 16:43:11 +0800

    Update 2-20.js

--- 2-20.js
+++ 2-20.js
@@ -10,9 +10,6 @@ function addAqiData() {
   document.getElementById('aqi-city-input').value = document.getElementById('aqi-value-input').value = ''
 }
 
-/**
- * 渲染aqi-table表格
- */
 function renderAqiList() {
   if (Object.keys(aqiData).length === 0) {
     document.getElementById('aqi-table').innerHTML = '';
@@ -26,19 +23,13 @@ function renderAqiList() {
   document.getElementById('aqi-table').innerHTML = tableStr;
 }
 
-/**
- * 点击add-btn时的处理逻辑
- * 获取用户输入，更新数据，并进行页面呈现的更新
- */
+
 function addBtnHandle() {
   addAqiData();
   renderAqiList();
 }
 
-/**
- * 点击各个删除按钮的时候的处理逻辑
- * 获取哪个城市数据被删，删除数据，更新表格显示
- */
+
 function delBtnHandle(city) {
   // do sth.
   delete aqiData[city]
@@ -47,11 +38,10 @@ function delBtnHandle(city) {
 
 function init() {
 
-  // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
   document.getElementById("add-btn").addEventListener("click", _ => {
     addBtnHandle()
   })
-  // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+
   document.getElementById("aqi-table").addEventListener("click", function(e){
     if(e.target.nodeName.toLowerCase() === 'button') {
       delBtnHandle(e.target.dataset.cityname);
